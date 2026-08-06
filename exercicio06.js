d155e383095aa09c5d4378de6b08233d8d4693b7
heloisalaves <heloisa.s.alves@edu.senai.br>
2026-08-06 15:52:18 -0300

    exercio06.js

---
+++ exercicio06.js
@@ -0,0 +1,17 @@
+const idades = [12, 18, 25, 40, 16, 22];
+console.log("Menores de idade: ")
+for (i = 0; i < idades.length; i++) {
+    if (idades[i] < 18) {
+        console.log(idades[i]);
+    }
+}
+
+console.log()
+
+console.log("Maiores de idade: ")
+for (i = 0; i < idades.length; i++) {
+    if (idades[i] >= 18) {
+        console.log(idades[i]);
+    }
+}
+
