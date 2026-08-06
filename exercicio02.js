1f70d5780230a1ad561291a5fd59bfa32e7b2e4a
heloisalaves <heloisa.s.alves@edu.senai.br>
2026-08-06 15:56:25 -0300

    feat: adiciona exercício 02

--- exercicio02.js
+++ exercicio02.js
@@ -1,7 +1,9 @@
+//Maior Número
 const numero = [12, 67, 34, 89, 21];
 let maior = 0
-for (let i = 0; i < numero.length; i++)
+for (let i = 0; i < numero.length; i++){
     if (numero[i] > maior) {
-        maior = numero[i]
+        maior = numero[i];
     }
-    console.log(maior)
+}
+    console.log(maior);
