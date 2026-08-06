e64aba4f7b83841c4123f18cb8695ca61e3a4af4
heloisalaves <heloisa.s.alves@edu.senai.br>
2026-08-06 15:39:27 -0300

    exercicio05.js

---
+++ exercicio05.js
@@ -0,0 +1,8 @@
+const estoque = [200, 450, 100, 900, 300];
+let maior = 0
+for (let i = 0; i < estoque.length; i++){
+    if (estoque[i] > maior) {
+        maior = estoque[i]
+    }
+}
+    console.log(maior)
